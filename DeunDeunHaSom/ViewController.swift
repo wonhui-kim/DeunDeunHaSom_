9a23dd1e6d1172bb24741ec92c43b88f21e13434
wonhui-kim <0512tini@gmail.com>
2022-08-07 21:29:01 +0900

    [SETTING] xcode 프로젝트 생성 (#1)

---
+++ DeunDeunHaSom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeunDeunHaSom
+//
+//  Created by 김원희 on 2022/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
